07894382fe31a31f688c23dcda2102acbe3ff0e6
LayMorja <evil-corporaty@yandex.ru>
2022-03-14 09:02:41 +0300

    Изменил меню, добавил страницу директоров

---
+++ src/js/files/script.js
@@ -0,0 +1,33 @@
+// Подключение функционала "Чертогов Фрилансера"
+import { isMobile } from "./functions.js";
+// Подключение списка активных модулей
+// import { flsModules } from "./modules.js";
+
+window.onload = function () {
+   document.addEventListener("click", documentActions);
+
+   function documentActions(e) {
+      const targetElement = e.target;
+      if (window.innerWidth > 880 && isMobile.any()) {
+         if (targetElement.classList.contains('menu__arrow')) {
+            targetElement.closest('.menu__item').classList.toggle('_hover');
+         }
+         if (!targetElement.closest('.menu__item') && document.querySelectorAll('.menu__item._hover').length > 0) {
+            removeClasses(document.querySelectorAll('.menu__item._hover'), "_hover")
+         }
+      }
+   }
+}
+
+
+
+
+const menuLinks = document.querySelectorAll('.sub-menu__link');
+
+for (let index = 0; index < menuLinks.length; index++) {
+   const menuLink = menuLinks[index];
+   
+   menuLink.addEventListener('click', function() {
+      menuLink.classList.toggle('_active');
+   })
+}
